8e8eb62c19c2fa1eead262191576652e28127d61
Sergey <seleznev-krista@mail.ru>
2023-12-10 12:48:17 +0300

    feat: add VigenereCipheringMachine base encryption

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(revers){
+    this.revers = revers
+    this.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+ // encrypt method accepts 2 parameters: message (string to encode) and key (string-keyword).
+
+
+ encrypt(message, key) {
+  if (!message || !key) throw new Error('Incorrect arguments!')
+  message = message.toUpperCase();
+  //key = key.toUpperCase()
+  key = key.toUpperCase().repeat(Math.trunc(message.length/key.length + 1)).slice(0, message.length);
+  let result = '';
+  let count = 0;
+  for (let i = 0; i < message.length; i++){
+      let curentChar = message[i]
+      let index = this.chars.indexOf(curentChar);
+      if (index < 0) result += curentChar;
+      else{
+          index += this.chars.indexOf(key[count]);
+          //console.log(index, index %  this.chars.length)
+          index = index %  this.chars.length;
+       //   console.log(index, this.chars[index],  message.indexOf(key[count]))
+          result += this.chars[index];
+          count++
+      }
+      // if (message[i].in)
+      // let char = chars[]
+  }
+  return result
+  //console.log('m:', message, 'key:',key, 'result:',result)
+  //throw new NotImplementedError('Not implemented');
+  //'AEIHQX SX DLLU!'
+  // remove line with error and write your code here
+}
+ // decrypt method accepts 2 parameters: encryptedMessage (string to decode) and key (string-keyword).
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!')
+   // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
