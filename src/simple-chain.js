0052fd437d55aedf0995af5a4126f9092ee73177
Sergey <seleznev-krista@mail.ru>
2023-12-15 13:07:31 +0300

    feat: add Make chain! ✔ chaining works! ✔ throws an Error

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,25 +6,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 const chainMaker = {
   result: '',
+  arr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    return this.arr.length;
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink( value ) {
+    this.arr.push(value)
+    return this
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink( position ) {
+    console.debug(position, this.arr, this.arr.length)
+    if (!Number(position) || position < 1 || position > this.arr.length  ) {
+      console.debug(position, 'ошибка')
+      this.arr = []
+      throw new Error("You can\'t remove incorrect link!")}
+    this.arr.splice(position - 1, 1);
+    return this;
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    this.arr.reverse()
+    return this;
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   finishChain() {
+    this.result = `( ${this.arr.join(' )~~( ')} )`;
     //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+   // console.debug(this.result)
+   this.arr = [];
     return this.result;
   }
 };
@@ -32,3 +48,11 @@ const chainMaker = {
 module.exports = {
   chainMaker
 };
+
+//console.log('test')
+//console.debug(chainMaker.addLink(1).addLink(2).addLink(3).removeLink(0)) 
+//console.debug(chainMaker.addLink(1).addLink(2).addLink(3).removeLink('2nd')) 
+//console.debug(chainMaker.addLink(1).addLink(2).addLink(3).removeLink(-2)) 
+//console.debug(chainMaker.addLink(1).addLink(2).addLink(3).removeLink(4)) 
+//chainMaker.addLink(function () { }).addLink('2nd').addLink('3rd').removeLink(2).reverseChain().finishChain()
+//, '( 3rd )~~( function () { } )');
